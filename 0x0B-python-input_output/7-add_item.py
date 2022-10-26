2295d0a8fafa2ba502f13edc01e048c5d810969c
ondiekmaxi <ondiekmaxwell498@gmail.com>
2022-10-26 14:44:56 -0700

    task io7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+cript adds all arguments to a Python list, and then save them to a file
+"""
+
+
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+MyList = argv[1:]
+try:
+    my_obj = load_from_json_file(filename)
+    my_obj += MyList
+    save_to_json_file(my_obj, filename)
+except:
+    my_obj = MyList
+    save_to_json_file(my_obj, filename)
